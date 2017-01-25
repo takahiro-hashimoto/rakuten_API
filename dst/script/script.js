45b16a84bfadfe0de4b8af864582277462000762
hashimoto <bridge.takahiro.1539@gmail.com>
2017-01-26 05:14:09 +0900

    script,style修正

--- dst/script/script.js
+++ dst/script/script.js
@@ -1,24 +1,24 @@
-
-var Items = []
+var Items;
 
 //キーワードを含む商品を検索
 function searchItem(keyword,page) {
   if (!keyword) {
-    return false;
+  return false;
   }
   $.ajax({
-    type: 'GET',
-    url: 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20130424',
-    data: {
-      applicationId: '1031022262194317111',
-      affiliateId: '146d79aa.7a7900a1.146d79ab.cab55273',
-      keyword: keyword,
-      page: page
-    }
+  type: 'GET',
+  url: 'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20130424',
+  data: {
+  applicationId: '1031022262194317111',
+  affiliateId: '146d79aa.7a7900a1.146d79ab.cab55273',
+  keyword: keyword,
+  page: page
+  }
   }).done(function (data) {
-      createItems(data);
-      Items.push(data);
-      hideLoader();
+  createItems(data);
+  hideLoader();
+  Items = data;
+  console.log(data);
   });
 }
 
@@ -30,39 +30,73 @@ function createItems(data) {
   var current = data.page;
   var dataStat = data.count;
   if (dataStat > 0) {
-    $.each(data.Items, function (i, items) {
-      var item = items.Item;
-      var affiliateUrl = item.affiliateUrl;
-      var imageUrl = item.mediumImageUrls[0].imageUrl;
-      var itemName = item.itemName;
-      if (itemName.length > 50) {
-        itemName = itemName.substring(0, 50) + '...';
+  $.each(data.Items, function (i, items) {
+  var item = items.Item;
+  var affiliateUrl = item.affiliateUrl;
+  var imageUrl = item.mediumImageUrls[0].imageUrl;
+  var itemName = item.itemName;
+  if (itemName.length > 50) {
+  itemName = itemName.substring(0, 50) + '...';
   　}
-      var itemPrice = item.itemPrice;
-      var itemReviewAverage = item.reviewAverage;
-      var itemShopName = item.shopName;
-      var htmlTemplate = $('<div class="card">' +
-          '<div class="card-inner">' +
-          '<div class="card-thumbnail-wrapper l-card-bottom-small">' +
-          '<a href="' + affiliateUrl + '">' +
-          '<img class="card-thumbnail" src="' + imageUrl + '" alt="' + item.itemName + '" width="150" ' +
-          'height="150"/>' +
-          '</a></div>' +
-          '<h2 class="card-title l-card-bottom-small"><a href="' + affiliateUrl + '">' + itemName + '</a></h2>' +
-          '<div class="card-shop">' + itemShopName + '</div>' +
-          '<div class="card-price">' + itemPrice + '円</div>' +
-          '<div class="card-review"><p>評価：' + itemReviewAverage + '点</p></div>' +
-          '</div>' +
-          '</div>');
+  var itemPrice = item.itemPrice;
+  var itemReviewAverage = item.reviewAverage;
+  var itemShopName = item.shopName;
+  var htmlTemplate = $('<div class="card">' +
+  '<div class="card-inner">' +
+  '<div class="card-thumbnail-wrapper l-card-bottom-small">' +
+  '<a href="' + affiliateUrl + '">' +
+  '<img class="card-thumbnail" src="' + imageUrl + '" alt="' + item.itemName + '" width="150" ' +
+  'height="150"/>' +
+  '</a></div>' +
+  '<h2 class="card-title l-card-bottom-small"><a href="' + affiliateUrl + '">' + itemName + '</a></h2>' +
+  '<div class="card-shop l-card-bottom-small">' + itemShopName + '</div>' +
+  '<div class="clearfix">' +
+  '<div class="l-right"><p class="card-price text-price text-strong">' + itemPrice + '円</p></div>' +
+  '<div class="l-left"><p class="card-review text-review">評価：' + '<span class="text-strong">' + itemReviewAverage + '点</span></p></div>' +
+  '</div>' +
+  '</div>' +
+  '</div>');
 
-      //テンプレートを追加
-      $('#js−search-result').append(htmlTemplate);
-    });
+  //テンプレートを追加
+  $('#js−search-result').append(htmlTemplate);
+  });
   }
 }
 
-$('.js-sort-review-score-high').on('click', function(){
-  //ここでItemsにいれたデータを並び替えて再度　createItems(data)を実行したいです。
+$(document).on('click', '.js-sort-review-score-high', function(){
+  if(Items) {
+  Items.Items.sort(function(a, b) {
+  return b.Item.reviewAverage - a.Item.reviewAverage;
+  });
+  createItems(Items);
+  }
+});
+
+$(document).on('click', '.js-sort-review-score-low', function(){
+  if(Items) {
+  Items.Items.sort(function(a, b) {
+  return a.Item.reviewAverage - b.Item.reviewAverage;
+  });
+  createItems(Items);
+  }
+});
+
+$(document).on('click', '.js-sort-price-high', function(){
+  if(Items) {
+  Items.Items.sort(function(a, b) {
+  return b.Item.itemPrice - a.Item.itemPrice;
+  });
+  createItems(Items);
+  }
+});
+
+$(document).on('click', '.js-sort-price-low', function(){
+  if(Items) {
+  Items.Items.sort(function(a, b) {
+  return a.Item.itemPrice - b.Item.itemPrice;
+  });
+  createItems(Items);
+  }
 });
 
 // Loadingイメージの表示
@@ -78,23 +112,23 @@ function hideLoader(){
 $(function (e) {
 
   $('#js-submit-button-top').on('click', function(e){
-    var keyword = $('#js-search-window-top').val();
-    if (!keyword) {
-      return false;
-    }
-    searchItem(keyword);
-    showLoader();
-    e.preventDefault();
+  var keyword = $('#js-search-window-top').val();
+  if (!keyword) {
+  return false;
+  }
+  searchItem(keyword);
+  showLoader();
+  e.preventDefault();
   });
 
   $('#js-submit-button-scroll').on('click', function(e){
-    var keyword = $('#js-search-window-scroll').val();
-    if (!keyword) {
-      return false;
-    }
-    searchItem(keyword);
-    showLoader();
-    e.preventDefault();
+  var keyword = $('#js-search-window-scroll').val();
+  if (!keyword) {
+  return false;
+  }
+  searchItem(keyword);
+  showLoader();
+  e.preventDefault();
   });
 
   //スティッキーヘッダー
@@ -102,13 +136,13 @@ $(function (e) {
   var $scrollHeader = $('#js-scroll-header');
 
   $window.on("scroll", function () {
-    if ($window.scrollTop() > 160) {
-      $scrollHeader.slideDown('fast');
-      toggleFooter();
-    } else {
-      $scrollHeader.slideUp('fast');
-      toggleFooter();
-    }
+  if ($window.scrollTop() > 160) {
+  $scrollHeader.slideDown('fast');
+  // toggleFooter();
+  } else {
+  $scrollHeader.slideUp('fast');
+  // toggleFooter();
+  }
   });
 
   $window.trigger('scroll');
